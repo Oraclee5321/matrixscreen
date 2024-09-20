7630a0b1e488acac59087b9c13f719fa16457d45
Alex Cooper <cooperalex5321@gmail.com>
2024-09-20 15:25:14 +0100

    Test

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-require("component")
+local components = require("component")
 function getMatrix ()
     local maddress = ""
     for address,name in component.list("induction_matrix",true) do
@@ -14,15 +14,19 @@ function getCurrentPower (matrix)
 end
 
 function getMaxPower (matrix)
-    local maxpower = component.invoke(matrix,"getMaxPower")
+    local maxpower = component.invoke(matrix,"getMaxEnergy")
     maxpower = maxpower * 4
     return maxpower
 end
 local charts = require("charts")
+local term = require("term")
+local event = require("event")
 local matrix = getMatrix()
 local gpu = component.gpu
 local w,h = gpu.getResolution()
-local powerbar = charts.container {
+
+
+local pbarleft = charts.Container {
     x = 1,
     y = 1,
     width = 50,
@@ -30,12 +34,12 @@ local powerbar = charts.container {
     payload = charts.ProgressBar{
         direction = charts.sides.LEFT,
         max = getMaxPower(matrix),
-        value=0,
+        value=getCurrentPower(matrix),
         colorFunc = function(_, perc)
             if perc >= .9 then
                 return 0x20afff
             elseif perc >= .75 then
-                return 0x20ff20
+                return 0x20ff2
             elseif perc >= .5 then
                 return 0xafff20
             elseif perc >= .25 then
@@ -48,3 +52,62 @@ local powerbar = charts.container {
         end
     }
 }
+
+local pbarright = charts.Container {
+    x = 1,
+    y = 4,
+    width = 50,
+    height = 2,
+    payload = charts.ProgressBar {
+      direction = charts.sides.RIGHT,
+      value = 0,
+      colorFunc = cleft.payload.colorFunc
+    }
+  }
+
+  local pbartop = charts.Container {
+    x = 55,
+    y = 1,
+    width = 2,
+    height = 20,
+    payload = charts.ProgressBar {
+      direction = charts.sides.TOP,
+      value = 0,
+      colorFunc = cleft.payload.colorFunc
+    }
+  }
+
+  local pbarbottom = charts.Container {
+    x = 59,
+    y = 1,
+    width = 2,
+    height = 20,
+    payload = charts.ProgressBar {
+      direction = charts.sides.BOTTOM,
+      value = 0,
+      colorFunc = cleft.payload.colorFunc
+    }
+  }
+  for i = 0, 100, 1 do
+      term.clear()
+      cleft.gpu.set(5, 10, "Value: " .. ("%.2f"):format(i / 100) .. " [" .. ("%3d"):format(i) .. "%]")
+      cleft.gpu.set(5, 11, "Max:   " .. cleft.payload.min)
+      cleft.gpu.set(5, 12, "Min:   " .. cleft.payload.max)
+
+      cleft.payload.value, cright.payload.value, ctop.payload.value, cbottom.payload.value = i / 100, i / 100, i / 100, i / 100
+
+      cleft:draw()
+      ctop:draw()
+      cright:draw()
+      cbottom:draw()
+
+      if event.pull(0.05, "interrupted") then
+        term.clear()
+        os.exit()
+      end
+    end
+
+    event.pull("interrupted")
+    term.clear()
+
+
