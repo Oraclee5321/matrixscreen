e320fbed997633a0d2d01d0b94870bf2521887c9
Alex Cooper <98158236+Oraclee5321@users.noreply.github.com>
2024-09-26 11:46:40 +0100

    Update main.lua
    Added drawData function.
    Added repeat until statement for constant update on the progress bar.

--- main.lua
+++ main.lua
@@ -1,28 +1,40 @@
 local components = require("component")
 function getMatrix ()
     local maddress = ""
-    for address,name in component.list("induction_matrix",true) do
+    for address,name in components.list("induction_matrix",true) do
         maddress = address
     end
     return maddress
 end
 
 function getCurrentPower (matrix)
-    local storedpower = component.invoke(matrix,"getEnergy") -- Gets energy stored in EU
+    local storedpower = components.invoke(matrix,"getEnergy") -- Gets energy stored in EU
     storedpower = storedpower * 4 -- Convert to RF
     return storedpower
 end
 
 function getMaxPower (matrix)
-    local maxpower = component.invoke(matrix,"getMaxEnergy")
+    local maxpower = components.invoke(matrix,"getMaxEnergy")
     maxpower = maxpower * 4
     return maxpower
 end
+
+function drawData(matrix,term,pbar)
+    local energy = getCurrentPower(matrix)
+    term.clear()
+    pbar.gpu.set(5, 10, "Current Power: " .. ("%.2f"):format(energy))
+    pbar.gpu.set(5, 11, "          Max: " .. pbar.payload.max)
+
+    pbar.payload.value = energy
+
+    pbar:draw()
+end
+
 local charts = require("charts")
 local term = require("term")
 local event = require("event")
 local matrix = getMatrix()
-local gpu = component.gpu
+local gpu = components.gpu
 local w,h = gpu.getResolution()
 
 
@@ -53,19 +65,12 @@ local pbar = charts.Container {
     }
 }
 
-local energy = getCurrentPower(matrix)
-term.clear()
-pbar.gpu.set(5, 10, "Current Power: " .. ("%.2f"):format(energy)
-pbar.gpu.set(5, 11, "Max:   " .. pbar.payload.max)
 
-pbar.payload.value = energy
+repeat
+    drawData(matrix,term,pbar)
+until
+    event.pull(0.05, "interrupted")
 
-pbar:draw()
-
-if event.pull(0.05, "interrupted") then
-    term.clear()
-    os.exit()
-end
 
 event.pull("interrupted")
 term.clear()
